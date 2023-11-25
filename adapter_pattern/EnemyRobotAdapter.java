cbad3c4b93a88d123fbcc0391fe8cd3ca5a43798
Maheedhar18 <maheedharreddy18@gmail.com>
2023-11-24 16:44:20 -0800

    added Adpater pattern

---
+++ adapter_pattern/EnemyRobotAdapter.java
@@ -0,0 +1,27 @@
+package adapter_pattern;
+
+public class EnemyRobotAdapter implements EnemyAttacker {
+
+    EnemyRobot enemyRobot;
+
+    public EnemyRobotAdapter(EnemyRobot enemyRobot) 
+    {
+        this.enemyRobot = enemyRobot;
+    }
+
+    @Override
+    public void fireWeapon() {
+        enemyRobot.smashWithHands();    
+    }
+
+    @Override
+    public void driveForward() {
+        enemyRobot.walkForward();      
+    }
+
+    @Override
+    public void assignDriver(String driverName) {
+        enemyRobot.reactToHuman(driverName);  
+    }
+    
+}
---
+++ adapter_pattern/TestEnemyAttackers.java
@@ -0,0 +1,27 @@
+package adapter_pattern;
+
+public class TestEnemyAttackers {
+
+    public static void main(String[] args) {
+        EnemyTank tank = new EnemyTank();
+        EnemyRobot robo = new EnemyRobot();
+        EnemyAttacker roboAdapter = new EnemyRobotAdapter(robo);
+
+        System.out.println("The Robot");
+        robo.reactToHuman("Jerry");
+        robo.walkForward();
+        robo.smashWithHands();
+
+        System.out.println("The Tank");
+        tank.fireWeapon();
+        tank.driveForward();
+        tank.assignDriver("Jerry");
+
+        System.out.println("The Robot with Adapter");
+        roboAdapter.fireWeapon();
+        roboAdapter.driveForward();
+        roboAdapter.assignDriver("Jerry");
+
+    }
+    
+}
